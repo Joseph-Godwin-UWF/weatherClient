b23eeef9ba935972b252693a494cb8467d3761af
Joseph-Godwin-UWF <77811961+Joseph-Godwin-UWF@users.noreply.github.com>
2022-02-17 13:04:06 -0600

    Add files via upload

--- user.cpp
+++ user.cpp
@@ -1,19 +1,19 @@
 #include "user.hpp"
 
 User::User(string username, int socket){
-	this.username = username;
-	this.socket = socket;
+	this->username = username;
+	this->socket = socket;
 }
 
 string User::getLocation(int index){
-	if(!(index >= this.locations.size()))
-		return this.locations.at(index);
+	if(!(index >= this->locations.size()))
+		return this->locations.at(index);
 	return "[-] INVALID INDEX FOR getLocation(int index)\n";
 }
 
 string User::getMessage(int index){
-	if(!(index >= this.messages.size()))
-		return this.messages.at(index);
+	if(!(index >= this->messages.size()))
+		return this->messages.at(index);
 	return "[-] INVALID INDEX FOR getMessage(int index)\n";
 }
 
@@ -25,15 +25,15 @@ void User::setSocket(int socket){
 }
 
 void User::addLocation(string location){
-	if(count(this.locations.begin(), this.locations.end(), location))
+	if(count(this->locations.begin(), this->locations.end(), location))
 		return;
-	this.locations.push_back(location);
+	this->locations.push_back(location);
 	return;
 }
 void User::removeLocation(string location){
-	for(int i = 0; i < this.locations.size(), i++){
-		if(location == this.locations.at(i)){
-			this.locations.erase(i);
+	for(int i = 0; i < this->locations.size(), i++){
+		if(location == this->locations.at(i)){
+			this->locations.erase(i);
 			return;
 		}
 		printf("vector<string> location does not contain %s\n", location);
@@ -42,15 +42,15 @@ void User::removeLocation(string location){
 }
 
 void User::addMessage(string message){
-	if(count(this.messages.begin(), this.messages.end(), message))
+	if(count(this->messages.begin(), this->messages.end(), message))
 		return;
-	this.messages.push_back(message);
+	this->messages.push_back(message);
 	return;
 }
 void User::removeMessage(string message){
-	for(int i = 0; i < this.messages.size(), i++){
-		if(message == this.messages.at(i)){
-			this.messages.erase(i);
+	for(int i = 0; i < this->messages.size(), i++){
+		if(message == this->messages.at(i)){
+			this->messages.erase(i);
 			return;
 		}
 		printf("vector<string> message does not contain %s\n", message);
